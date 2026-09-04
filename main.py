bc8da1a34b96f51e856786244179b31a78e23bca
Sandeep Kumar <sandeepspk1797@gmail.com>
2026-09-04 11:29:38 +0530

    cors fixed

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ app = FastAPI(title="AI Real Estate Recommendation API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=["https://d1044jhuklqhnb.cloudfront.net"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
